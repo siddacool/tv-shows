b997b4d740481bdca70973c1f84717c8da1ad135
Siddhesh Mangela <sidthecool007@gmail.com>
2017-11-03 15:07:02 +0530

    unused clutter removed

--- resources/js/custom-methods/load-svg-icon.js
+++
@@ -1,9 +0,0 @@
-function icon(name) {
-  return `
-    <svg role="img" class="icon">
-    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-${name}"></use>
-    </svg>
-  `;
-}
-
-export default icon;
